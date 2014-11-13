a5c9fecf1637394d395fcc7578ca6d6446eb1ab0
Corey Quinn <corey@sequestered.net>
2014-11-13 15:26:26 -0800

    Initial commit with Tim's script

---
+++ tallen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+logdate=$(date +"%Y%m%d")
+logfile="$logdate"_repoert.log
+while ps -e | grep bonnie++ > /dev/null
+do
+       free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+       df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
+       top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}'
+       sleep 10
+done
+       echo "bonnie++ is no longer running"
+
+cat <<- EOF > /test/$logfile
+       This report was generated while running bonnie++.
+       It contains CPU, Memory, and Disk load while bonnie++ was running.
+EOF
